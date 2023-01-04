c6a7b63191bf3f756e1c10f1f06f2c26575a09d6
JULICE KIM <julicekim@JULICEui-MacBookPro-2.local>
2023-01-05 07:14:41 +0900

    최초 생성

---
+++ frontend/src/App.svelte
@@ -0,0 +1,12 @@
+<script>
+  let message;
+
+  fetch("http://localhost:8000/hello")
+    .then((resp) => {
+        resp.json().then((json) => {
+            message = json.message;
+          })
+      })
+</script>
+
+<h1>{message}</h1>
